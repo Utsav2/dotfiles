2cb30732f4c900f88112ab038719fb1cb1aefbcc
Utsav2 <ukshah2@illinois.edu>
2016-02-01 21:28:29 -0600

    Simplify install script

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
 sudo pacman -S yaourt
-
 yaourt -S zsh
-
-# control p
-mkdir -p ~/.vim
-cd ~/.vim
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
-
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
-echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> ~/.vimrc
+ln -s ~/.spacemacs .spacemacs
